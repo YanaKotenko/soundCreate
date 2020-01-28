aab6cdce7cc137386d3d0afc4e24c94a284e3f71
yana kotenko <yana.kotenko@waveaccess.ru>
2020-01-28 23:50:13 +0300

    Make start for further animation equalizer

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,17 @@
 <template lang='pug'>
 .contact
+  .contact_equalizer
+    .contact_equalizer__line._line1
+    .contact_equalizer__line._line2
+    .contact_equalizer__line._line3
+    .contact_equalizer__line._line4
+    .contact_equalizer__line._line5
+    .contact_equalizer__line._line6
+    .contact_equalizer__line._line7
+    .contact_equalizer__line._line8
+    .contact_equalizer__line._line9
+    .contact_equalizer__line._line10
+  
   .contact_title 
     | Let’s make
     span something
@@ -25,6 +37,42 @@
     justify-content: center
     // высота контентной части страниц за вычетом высоты хедера и футера
     min-height: calc(100vh - 8vh - 7vh)
+
+    &_equalizer
+
+      &__line
+        width: 4px
+        height: 130px
+        background: $darkGrey
+        display: inline-block
+        margin-right: 2px
+        animation-iteration-count: infinite
+        animation-direction: alternate
+        animation-duration: 5s
+        animation-timing-function: linear
+        animation-name: equalizer
+        animation-fill-mode: forwards
+
+        &._line1
+          animation-duration: 5.6s
+        &._line2
+          animation-duration: 5s
+        &._line3
+          animation-duration: 6s
+        &._line4
+          animation-duration: 2s
+        &._line5
+          animation-duration: 4s
+        &._line6
+          animation-duration: 3s
+        &._line7
+          animation-duration: 1s
+        &._line8
+          animation-duration: 4.3s
+        &._line9
+          animation-duration: 2.7s
+        &._line10
+          animation-duration: 4.9s
 
     &_title
       font-weight: 600
@@ -52,5 +100,12 @@
       color: $white
       text-decoration: none
 
-    
+  @keyframes equalizer
+    0%
+      transform: scaleY(1)
+    50%
+      transform: scaleY(.5)
+    100%
+      transform: scaleY(1)
+
 </style>
